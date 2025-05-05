796d41621b12fddbf2bbb4aecb581264dd3b95f8
HamzaSeyfu <165190579+HamzaSeyfu@users.noreply.github.com>
2025-05-05 11:46:51 +0200

    Update delete-5gc.sh

--- scripts/delete-5gc.sh
+++ scripts/delete-5gc.sh
@@ -2,14 +2,14 @@
 
 echo "Suppression of the deploiement 5GC..."
 
-kubectl delete -f upf-service.yaml --ignore-not-found
-kubectl delete -f smf-service.yaml --ignore-not-found
-kubectl delete -f amf-service.yaml --ignore-not-found
+kubectl delete -f manifests/upf-service.yaml --ignore-not-found
+kubectl delete -f manifests/smf-service.yaml --ignore-not-found
+kubectl delete -f manifests/amf-service.yaml --ignore-not-found
 
-kubectl delete -f upf-deployment.yaml --ignore-not-found
-kubectl delete -f smf-deployment.yaml --ignore-not-found
-kubectl delete -f amf-deployment.yaml --ignore-not-found
+kubectl delete -f manifests/upf-deployment.yaml --ignore-not-found
+kubectl delete -f manifests/smf-deployment.yaml --ignore-not-found
+kubectl delete -f manifests/amf-deployment.yaml --ignore-not-found
 
-kubectl delete -f namespace.yaml --ignore-not-found
+kubectl delete -f manifests/namespace.yaml --ignore-not-found
 
 echo "Everything is clean !"
