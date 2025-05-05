bc328e3386f801c3799fa2e2a62acad9af91dae5
HamzaSeyfu <165190579+HamzaSeyfu@users.noreply.github.com>
2025-05-05 11:45:36 +0200

    Update deploy-5gc.sh

--- scripts/deploy-5gc.sh
+++ scripts/deploy-5gc.sh
@@ -6,14 +6,14 @@ echo "Deploiement of the 5GC dans Kubernetes..."
 kubectl get ns 5gc >/dev/null 2>&1 || kubectl create namespace 5gc
 
 # Apply les YAMLs of deploiement
-kubectl apply -f namespace.yaml
-kubectl apply -f amf-deployment.yaml
-kubectl apply -f smf-deployment.yaml
-kubectl apply -f upf-deployment.yaml
+kubectl apply -f manifests/namespace.yaml
+kubectl apply -f manifests/amf-deployment.yaml
+kubectl apply -f manifests/smf-deployment.yaml
+kubectl apply -f manifests/upf-deployment.yaml
 
 # Appliquer les services (if not separated)
-kubectl apply -f amf-service.yaml
-kubectl apply -f smf-service.yaml
-kubectl apply -f upf-service.yaml
+kubectl apply -f manifests/amf-service.yaml
+kubectl apply -f manifests/smf-service.yaml
+kubectl apply -f manifests/upf-service.yaml
 
 echo "Deploiement succesful. Vérifie the pods with : kubectl get pods -n 5gc"
